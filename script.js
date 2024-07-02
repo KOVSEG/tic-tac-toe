2631cedc305a846fe3625cf33223a970d8f14f96
Kovseg <vladislavkoritskiy@yandex.ru>
2024-07-02 16:21:03 +0300

    added buttons of shapes and simple logic for game, v_0_0_4

--- script.js
+++ script.js
@@ -1,8 +1,38 @@
-let getTd = document.getElementsByTagName('td');
+function startGame() {
+  let getTd = document.getElementsByTagName('td');
+  let shapes = document.querySelector('.shapes');
 
-for(let i = 0; i < getTd.length; i++) {
-  let td = getTd[i];
-  td.addEventListener('click', function(event) {
-    console.log(event.target)
+  shapes.addEventListener('click', function getShape(event) {
+    let shape;
+    let sign = event.target;
+    sign = sign.id;
+    if(sign === 'cross-btn') {
+      shape = 'cross';
+    } else {
+      shape = 'ring';
+    }
+
+    this.removeEventListener('click', getShape);
+
+    for(let i = 0; i < getTd.length; i++) {
+      let td = getTd[i];
+      td.addEventListener('click', function(event) {
+        event.target.classList.add(setShape(shape));
+      });
+    }
+  
+    function setShape(getValue) {
+      if(getValue === 'ring') {
+        shape = 'cross';
+        return 'cross';
+      } else {
+        shape = 'ring';
+        return 'ring';
+      }
+    };
+
   });
-}
+
+};
+
+startGame();
