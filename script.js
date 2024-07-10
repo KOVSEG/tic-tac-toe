c136cd47208cde5d5b4dac43706d20f55da0ac02
Kovseg <vladislavkoritskiy@yandex.ru>
2024-07-10 11:46:10 +0300

    found a path to comparison winner combination with getting combination of shapes, v_0_0_6

--- script.js
+++ script.js
@@ -2,6 +2,18 @@ function startGame() {
   let getTd = document.getElementsByTagName('td');
   let shapes = document.querySelector('.shapes');
   let counter = 0;
+  let crossCombination = [];
+  let ringCombination = [];
+  let winCombinations = [
+    ['00', '01', '02'], 
+    ['10', '11', '12'],
+    ['20', '21', '22'],
+    ['00', '10', '20'],
+    ['01', '11', '21'],
+    ['02', '12', '22'],
+    ['00', '11', '22'],
+    ['02', '11', '20']
+  ];
 
   shapes.addEventListener('click', function getShape(event) {
     let shape;
@@ -19,10 +31,19 @@ function startGame() {
       let td = getTd[i];
       td.addEventListener('click', function setShape(event) {
         event.target.classList.add(chooseShape(shape));
+
+        // Save the id of every turns for definite shape
+        shape === 'cross' ? crossCombination.push(this.id) : ringCombination.push(this.id);
+
+        if(true) {
+        // if(crossCombination.length >= 3 || ringCombination.length >= 3) {
+          checkCombinations();
+        }
+
         this.removeEventListener('click', setShape);
       });
     }
-  
+
     function chooseShape(getValue) {
       if(counter === 0) {
         shape = getValue;
@@ -39,6 +60,41 @@ function startGame() {
 
   });
 
+
+  function checkCombinations() {
+    console.log('Крестики: ' + crossCombination)
+    console.log('Нолики: ' + ringCombination)
+  };
+
 };
 
-startGame();
+// startGame();
+
+
+
+
+let winCombinations = [
+  // ['00', '01', '02'],
+  // ['10', '11', '12'],
+  // ['20', '21', '22'],
+  ['00', '10', '20']
+  // ['01', '11', '21'],
+  // ['02', '12', '22'],
+  // ['00', '11', '22'],
+  // ['02', '11', '20']
+];
+
+
+// let array = ['00','10','20'];
+let array = ['20','10','00'];
+
+
+for(let i = 0; i < winCombinations.length; i++) {
+  for(let j = 0; j < array.length; j++) {
+    console.log(winCombinations[i].indexOf(array[j]));
+    if(winCombinations[i].indexOf(array[j]) != -1) {
+      console.log(`id: ${winCombinations[i]} has ${array[j]}`);
+    }
+  }
+}
+
