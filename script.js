72484cf7b2851a9c63470e9d6fc0f0bcc95a9f15
Kovseg <vladislavkoritskiy@yandex.ru>
2024-07-12 17:13:43 +0300

    changed checkCombinations func and added commonFunc for combination matches

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ function startGame() {
   let getTd = document.getElementsByTagName('td');
   let shapes = document.querySelector('.shapes');
   let counter = 0;
+  let attempt = 0;
   let crossCombination = [];
   let ringCombination = [];
   let winCombinations = [
@@ -31,13 +32,21 @@ function startGame() {
       let td = getTd[i];
       td.addEventListener('click', function setShape(event) {
         event.target.classList.add(chooseShape(shape));
+        attempt++;
 
         // Save the id of every turns for definite shape
         shape === 'cross' ? crossCombination.push(this.id) : ringCombination.push(this.id);
+        let shapeTranslate;
+        if(shape === 'cross') {
+          shapeTranslate = 'Крестики';
+        } else {
+          shapeTranslate = 'Нолики';
+        }
 
-        if(true) {
-        // if(crossCombination.length >= 3 || ringCombination.length >= 3) {
-          checkCombinations();
+        if (checkCombinations(shape)) {
+          alert(`Выиграли - ${shapeTranslate}`);
+        } else if (attempt === 9) {
+          alert('Ничья');
         }
 
         this.removeEventListener('click', setShape);
@@ -60,41 +69,29 @@ function startGame() {
 
   });
 
+  function checkCombinations(getShapes) {
+    if(getShapes === 'cross') {
+      return commonFunc(crossCombination);
+    } else {
+      return commonFunc(ringCombination);
+    }
+  };
 
-  function checkCombinations() {
-    console.log('Крестики: ' + crossCombination)
-    console.log('Нолики: ' + ringCombination)
+  function commonFunc(combination) {
+    for(let i = 0; i < winCombinations.length; i++) {
+      let combinationMatch = 0;
+      for(let j = 0; j < winCombinations[i].length; j++) {
+        if(combination.indexOf(winCombinations[i][j]) != -1) {
+          combinationMatch++;
+          if(combinationMatch === 3) {
+            return true;
+          }
+        }
+      }
+    }
+    return false;
   };
 
 };
 
-// startGame();
-
-
-
-
-let winCombinations = [
-  // ['00', '01', '02'],
-  // ['10', '11', '12'],
-  // ['20', '21', '22'],
-  ['00', '10', '20']
-  // ['01', '11', '21'],
-  // ['02', '12', '22'],
-  // ['00', '11', '22'],
-  // ['02', '11', '20']
-];
-
-
-// let array = ['00','10','20'];
-let array = ['20','10','00'];
-
-
-for(let i = 0; i < winCombinations.length; i++) {
-  for(let j = 0; j < array.length; j++) {
-    console.log(winCombinations[i].indexOf(array[j]));
-    if(winCombinations[i].indexOf(array[j]) != -1) {
-      console.log(`id: ${winCombinations[i]} has ${array[j]}`);
-    }
-  }
-}
-
+startGame();
